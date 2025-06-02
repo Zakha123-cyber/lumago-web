0cf1a37fbf7d579f31aff49e71fd098ce98fbb07
Zakha123-cyber <aguscow6@gmail.com>
2025-06-03 01:38:03 +0700

    set up breeze

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\WisataController;
 
-Route::get('/', [LandingController::class, 'index'])->name('landing.index');
-// Wisata Routes
-Route::prefix('wisata')->group(function () {
-    Route::get('/', [WisataController::class, 'index'])->name('wisata.index');
-    Route::get('/{id}', [WisataController::class, 'show'])->name('wisata.detail');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    //Landing Page
+    Route::get('/', [LandingController::class, 'index'])->name('landing.index');
+    //Detail Wisata
+    Route::prefix('wisata')->group(function () {
+        Route::get('/', [WisataController::class, 'index'])->name('wisata.index');
+        Route::get('/{id}', [WisataController::class, 'show'])->name('wisata.detail');
+    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/wisata', function () {
-    return view('welcome.landing');
-});
-
-Route::get('/booking', function () {
-    return view('booking-page.booking-page');
-});
-
-Route::get('/detail', function () {
-    return view('detail-page.detail-wisata');
-});
-
-Route::get('/profil', function () {
-    return view('profile-page.profile-page');
-});
+require __DIR__ . '/auth.php';
